9347662940b9ca24f1e3047d9b587f6793cad1f6
Devdha <france998811@gmail.com>
2023-01-06 23:05:04 +0900

    🐛 fix(backend/app): controller dependency error

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -5,7 +5,6 @@ import { UsersController } from './users/users.controller';
 import { EvaluationsController } from './evaluations/evaluations.controller';
 import { CoalitionsController } from './coalitions/coalitions.controller';
 import { SubjectsController } from './subjects/subjects.controller';
-import { UsersModule } from './coalitions/users/users.module';
 import { UsersModule } from './users/users.module';
 import { CoalitionsModule } from './coalitions/coalitions.module';
 import { EvaluationsModule } from './evaluations/evaluations.module';
@@ -13,13 +12,7 @@ import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
   imports: [UsersModule, CoalitionsModule, EvaluationsModule, SubjectsModule],
-  controllers: [
-    AppController,
-    UsersController,
-    EvaluationsController,
-    CoalitionsController,
-    SubjectsController,
-  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
